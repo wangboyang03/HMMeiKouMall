5bf5e8134a4f6e2cef78451917fca45e46e0ff22
wangboyang <18032391370@163.com>
2026-06-17 14:54:46 +0800

    feat(hmrouter): 初始化HMRouter插件配置，分别在hap/hsp/har包中配置插件并在UIAbility中通过上下文对象初始化HMRouter。

--- commons/basic/hvigorfile.ts
+++ commons/basic/hvigorfile.ts
@@ -1,6 +1,7 @@
+import { harPlugin } from '@hadss/hmrouter-plugin';
 import { harTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
   system: harTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [harPlugin()]       /* Custom plugin to extend the functionality of Hvigor. */
 }
--- products/phone/hvigorfile.ts
+++ products/phone/hvigorfile.ts
@@ -1,6 +1,7 @@
+import { hapPlugin } from '@hadss/hmrouter-plugin';
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
   system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  plugins: [hapPlugin()] /* HM Custom plugin to extend the functionality of Hvigor. */
 }
